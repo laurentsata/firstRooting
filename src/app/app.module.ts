fd03c35fa88799e0d8fff4cd97597597e0035609
laurentsata <sataward666@gmail.com>
2022-10-25 11:16:57 +0200

    exo quest Angular04

---
+++ src/app/app.module.ts
@@ -0,0 +1,23 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
+
+import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { ContactComponent } from './contact/contact.component';
+import { ROUTES } from './app.routes';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    ContactComponent
+  ],
+  imports: [
+    BrowserModule,
+    RouterModule.forRoot(ROUTES)
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
